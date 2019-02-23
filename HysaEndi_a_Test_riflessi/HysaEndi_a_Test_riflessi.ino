5afc9ae7b23ecb8f50b9bad9139248742de18bbe
Endi <endi.watch@gmail.com>
2019-02-23 09:00:22 +0100

    23/02/2019-- spiegazione bottoni in arduino

--- HysaEndi_a_Test_riflessi/HysaEndi_a_Test_riflessi.ino
+++ HysaEndi_a_Test_riflessi/HysaEndi_a_Test_riflessi.ino
@@ -1,13 +1,28 @@
+#define LED 13
+#define BUTTON 7
+int val = 0;
+
+
+
 void setup() {
   // put your setup code here, to run once:
  
-  pinMode (8, OUTPUT);
+  pinMode (LED, OUTPUT);
+  pinMode ( BUTTON, INPUT);
+  
 
 }  
 
 void loop() {
   // put your main code here, to run repeatedly:
+   val = digitalRead(BUTTON);
 
-
- 
+ if (val == HIGH)
+ {
+  digitalWrite (LED,HIGH);
+ }
+ else
+ {
+  digitalWrite (LED,LOW);
+ }
 }
