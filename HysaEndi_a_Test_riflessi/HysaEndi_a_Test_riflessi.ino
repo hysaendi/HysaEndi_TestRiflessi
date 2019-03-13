415d532c4e269c15fe49e7a75d68dce4422cdd2f
Endi <endi.watch@gmail.com>
2019-03-13 13:21:49 +0100

    13/03/2019 --- inizio scrittura programma

--- HysaEndi_a_Test_riflessi/HysaEndi_a_Test_riflessi.ino
+++ HysaEndi_a_Test_riflessi/HysaEndi_a_Test_riflessi.ino
@@ -1,28 +1,17 @@
-#define LED 13
-#define BUTTON 7
-int val = 0;
+
 
 
 
 void setup() {
   // put your setup code here, to run once:
  
-  pinMode (LED, OUTPUT);
-  pinMode ( BUTTON, INPUT);
+  
   
 
 }  
 
 void loop() {
   // put your main code here, to run repeatedly:
-   val = digitalRead(BUTTON);
+   
 
- if (val == HIGH)
- {
-  digitalWrite (LED,LOW);
- }
- else
- {
-  digitalWrite (LED,HIGH);
- }
-}
+ 
