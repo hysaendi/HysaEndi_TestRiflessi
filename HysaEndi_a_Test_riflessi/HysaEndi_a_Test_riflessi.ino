50f9545293865c5b5deb3c83d97e18910ded3086
Endi <endi.watch@gmail.com>
2019-03-13 13:40:02 +0100

    13/03/2019 --- dichiarazione variabili

--- HysaEndi_a_Test_riflessi/HysaEndi_a_Test_riflessi.ino
+++ HysaEndi_a_Test_riflessi/HysaEndi_a_Test_riflessi.ino
@@ -1,16 +1,21 @@
 int  ledBlu; //led primotest
 int buzzer; //secondo test
-int rgb_rosso; //led che cambia colore
-int rgb_verde; 
+int rgb_Rosso; //led che cambia colore
+int rgb_Verde; 
 int tempoTestIn;  //tempo prova led blu
 int tempoTestBuzz;  //tempo prova buzzer
+int bottone;  //bottone per esecuzione primo test
 
 
 
 
 void setup() {
   // put your setup code here, to run once:
- 
+ pinMode (bottone,INPUT);
+ pinMode (ledBlu, OUTPUT);
+ pinMode (buzzer, OUTPUT);
+ pinMode (rgb_Rosso, OUTPUT);
+ pinMode (rgb_Verde, OUTPUT);
   
   
 
