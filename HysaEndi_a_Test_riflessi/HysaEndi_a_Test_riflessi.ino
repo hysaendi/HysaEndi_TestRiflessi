bd924383e12a0d009491f522145e21bb1cb33278
Endi <endi.watch@gmail.com>
2019-03-15 20:23:39 +0100

    15/03/2019
    scrittura codice provvisorio, da testare il funzionamento

--- HysaEndi_a_Test_riflessi/HysaEndi_a_Test_riflessi.ino
+++ HysaEndi_a_Test_riflessi/HysaEndi_a_Test_riflessi.ino
@@ -4,7 +4,8 @@ int rgb_Rosso; //led che cambia colore
 int rgb_Verde; 
 int tempoTestIn;  //tempo prova led blu
 int tempoTestBuzz;  //tempo prova buzzer
-int bottone;  //bottone per esecuzione primo test
+int bottone;//bottone per esecuzione primo test
+int statoBottone; //stato del bottone: HIGH o LOW
 
 
 
@@ -23,6 +24,48 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+   premerePulsante();  //il programma parte con la pressione del bottone
+   tempoTestIn = esecuzioneTest (ledBlu, "Lasso di tempo riguardante il test visivo:");
+   tempoTestBuzz = esecuzioneTest (buzzer, "Lasso di tempo riguardante il test uditivo:");
+   verificaTest (tempoTestIn,tempoTestBuzz,rgb_Verde,rgb_Rosso);
+  
    
 
+}
+
+void premerePulsante()
+{ statoBottone = digitalRead (bottone); //variabile di appoggio
+  statoBottone = HIGH;
+}
+
+int esecuzioneTest (int oggetto, String messaggio)
+{
+  int tempoUno;
+  int tempoTestTot;
+  delay (random(500,8000));
+  digitalWrite (oggetto,HIGH);
+  tempoTestIn = millis(); //restituisce il tempo trascorso fino a quando non premo il pulsante
+  premerePulsante();
+  digitalWrite (oggetto,LOW);
+  int tempoDue; //dichiaro il secondo test
+  delay (random(500,8000));
+  digitalWrite (oggetto,HIGH);
+  tempoTestBuzz = millis();
+  tempoTestTot = tempoUno + tempoDue;
+  Serial.println (messaggio + String(tempoTestTot));
+}
+
+void verificaTest (int tempoTestIn, int tempoTestBuzz , int testSuperato, int testNonSuperato)
+{
+  //con test superato e non superato intendo di che colore il led rgb si dovrà colorare ( verde = test superato / rosso = test non superato)
+  if (tempoTestIn > 250 && tempoTestBuzz > 250)
+  {
+    digitalWrite (testNonSuperato,HIGH);
+  }
+  else
+  {
+    digitalWrite (testSuperato,HIGH);
+  }
+  digitalWrite (testSuperato,LOW);  //spengo i 2 led rgb
+  digitalWrite (testNonSuperato,LOW);
 }
